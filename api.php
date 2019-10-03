fad8d6061c114e4e448846cb3b892e4b05788f7f
Slushatel2019 <sluhach.nadpsu@gmail.com>
2019-10-03 12:38:00 +0300

    improve_redirect

--- api.php
+++ api.php
@@ -1,22 +1,25 @@
 <?php
 require_once('Books.php');
-if (!isset($_SERVER['PHP_AUTH_USER'])) {
+function get($value, $default = null)
+{
+    return isset($value) ? $value : $default;
+}
+$user = get($_SERVER['PHP_AUTH_USER'], false);
+$pass = get($_SERVER['PHP_AUTH_PW'], false);
+if (!$user or !$pass) {
     header('WWW-Authenticate: Basic realm');
 }
-if (Books::getInstance()->auth($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
-
-    if (preg_match("[^/api/books(|/)$]", $_SERVER['REQUEST_URI'])) {
-        Books::getInstance()->allBooks();
-    }
-    if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $_SERVER['REQUEST_URI'])) {
-        preg_match("/[0-9]{1,}/", $_SERVER['REQUEST_URI'], $matches);
-        Books::getInstance()->booksId($matches[0]);
-    }
-    if (preg_match("[^/api/books/count(|/)$]", $_SERVER['REQUEST_URI'])) {
-        Books::getInstance()->count();
-    }
+$login = Books::getInstance()->auth($user, $pass);
+if (!$login) {
+    header('WWW-Authenticate: Basic realm');
 }
-else {
-    echo 'incorrect login or password';
-    header('WWW-Authenticate: Basic realm'); 
+if (preg_match("[^/api/books(|/)$]", $_SERVER['REQUEST_URI'])) {
+    Books::getInstance()->allBooks();
+}
+if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $_SERVER['REQUEST_URI'])) {
+    preg_match("/[0-9]{1,}/", $_SERVER['REQUEST_URI'], $matches);
+    Books::getInstance()->booksId($matches[0]);
+}
+if (preg_match("[^/api/books/count(|/)$]", $_SERVER['REQUEST_URI'])) {
+    Books::getInstance()->count();
 }
