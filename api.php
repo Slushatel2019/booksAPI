e5260ff3d0166981e3c4282c2e8e4f021f500ea6
Slushatel2019 <sluhach.nadpsu@gmail.com>
2019-10-05 20:51:40 +0300

    in_process

--- api.php
+++ api.php
@@ -12,14 +12,46 @@ if (!$user or !$pass) {
 $login = Books::getInstance()->auth($user, $pass);
 if (!$login) {
     header('WWW-Authenticate: Basic realm');
+    exit('incorrect login or password');
 }
-if (preg_match("[^/api/books(|/)$]", $_SERVER['REQUEST_URI'])) {
-    Books::getInstance()->allBooks();
-}
-if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $_SERVER['REQUEST_URI'])) {
-    preg_match("/[0-9]{1,}/", $_SERVER['REQUEST_URI'], $matches);
-    Books::getInstance()->booksId($matches[0]);
-}
-if (preg_match("[^/api/books/count(|/)$]", $_SERVER['REQUEST_URI'])) {
-    Books::getInstance()->count();
+switch ($_SERVER['REQUEST_METHOD']) {
+    case "GET":
+        if (preg_match("[^/api/books(|/)$]", $_SERVER['REQUEST_URI'])) {
+            Books::getInstance()->allBooks();
+        }
+        if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $_SERVER['REQUEST_URI'])) {
+            preg_match("/[0-9]{1,}/", $_SERVER['REQUEST_URI'], $matches);
+            Books::getInstance()->booksId($matches[0]);
+        }
+        if (preg_match("[^/api/books/count(|/)$]", $_SERVER['REQUEST_URI'])) {
+            Books::getInstance()->count();
+        }
+        break;
+    case "POST":
+        if (preg_match("[^/api/books(|/)$]", $_SERVER['REQUEST_URI'])) {
+            $postdata = file_get_contents("php://input");
+            $array = json_decode($postdata, true);
+            if (is_array($array)) {
+                echo (Books::getInstance()->add($array));
+            }
+            echo 'data format is not an array';
+        }
+        echo 'incorrect URL';
+        break;
+    case "PUT":
+        $postdata = file_get_contents("php://input");
+        $array = json_decode($postdata, true);
+        if (is_array($array)) {
+            echo (Books::getInstance()->update($array, $_SERVER['REQUEST_URI']));
+            exit;
+        }
+        echo 'data format is not an array';
+        break;
+    case "DELETE":
+        if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $_SERVER['REQUEST_URI'])) {
+            echo (Books::getInstance()->del($_SERVER['REQUEST_URI']));
+            exit;
+        }
+        echo "incorrect url for delete";
+        break;
 }
