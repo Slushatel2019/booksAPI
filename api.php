1ef7f66ed7664de0db3d3e36e161b75449b2fc9f
Slushatel2019 <sluhach.nadpsu@gmail.com>
2019-10-08 14:44:38 +0300

    try

--- api.php
+++ api.php
@@ -8,6 +8,7 @@ $user = get($_SERVER['PHP_AUTH_USER'], false);
 $pass = get($_SERVER['PHP_AUTH_PW'], false);
 if (!$user or !$pass) {
     header('WWW-Authenticate: Basic realm');
+    exit('incorrect login or password');
 }
 $login = Books::getInstance()->auth($user, $pass);
 if (!$login) {
@@ -17,39 +18,64 @@ if (!$login) {
 switch ($_SERVER['REQUEST_METHOD']) {
     case "GET":
         if (preg_match("[^/api/books(|/)$]", $_SERVER['REQUEST_URI'])) {
-            Books::getInstance()->allBooks();
+            $result = Books::getInstance()->allBooks();
+            Books::getInstance()->response($result);
+            exit;
         }
         if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $_SERVER['REQUEST_URI'])) {
             preg_match("/[0-9]{1,}/", $_SERVER['REQUEST_URI'], $matches);
-            Books::getInstance()->booksId($matches[0]);
+            $result = Books::getInstance()->booksId($matches[0]);
+            Books::getInstance()->response($result);
+            exit;
         }
         if (preg_match("[^/api/books/count(|/)$]", $_SERVER['REQUEST_URI'])) {
-            Books::getInstance()->count();
+            $result = Books::getInstance()->count();
+            Books::getInstance()->response($result);
+            exit;
         }
         break;
     case "POST":
         if (preg_match("[^/api/books(|/)$]", $_SERVER['REQUEST_URI'])) {
             $postdata = file_get_contents("php://input");
-            $array = json_decode($postdata, true);
-            if (is_array($array)) {
-                echo (Books::getInstance()->add($array));
+            try {
+                $array = json_decode($postdata, true);
             }
-            echo 'data format is not an array';
+                catch (Exception $e) { 
+                    echo 'hello';
+                }
+            var_dump($array);
+            if (is_array($array)) {
+                $result = Books::getInstance()->add($array);
+                Books::getInstance()->response($result);
+                exit;
+            }
         }
-        echo 'incorrect URL';
+        exit('incorrect URL');
         break;
     case "PUT":
         $postdata = file_get_contents("php://input");
         $array = json_decode($postdata, true);
-        if (is_array($array)) {
-            echo (Books::getInstance()->update($array, $_SERVER['REQUEST_URI']));
+        $array = is_array($array) ? $array : exit('data format is not an array');
+        if (preg_match("[^/api/books(|/)$]", $_SERVER['REQUEST_URI'])) {
+            $result = Books::getInstance()->update($array);
+            Books::getInstance()->response($result);
             exit;
         }
-        echo 'data format is not an array';
+        if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $_SERVER['REQUEST_URI'])) {
+            preg_match_all('/[0-9]/', $_SERVER['REQUEST_URI'], $matches);
+            $id = implode($matches[0]);
+            $result = Books::getInstance()->update($array, $id);
+            Books::getInstance()->response($result);
+            exit;
+        }
+        exit('incorrect URL');
         break;
     case "DELETE":
         if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $_SERVER['REQUEST_URI'])) {
-            echo (Books::getInstance()->del($_SERVER['REQUEST_URI']));
+            preg_match_all('/[0-9]/', $_SERVER['REQUEST_URI'], $matches);
+            $id = implode($matches[0]);
+            $result = Books::getInstance()->del($id);
+            Books::getInstance()->response($result);
             exit;
         }
         echo "incorrect url for delete";
--- books.php
+++ books.php
@@ -17,7 +17,8 @@ class Books
     }
     public function auth($login, $password)
     {
-        $stmt = $this->link->prepare("SELECT id FROM users WHERE BINARY login = :login AND BINARY password = :password");
+        $stmt = $this->link->prepare("SELECT id FROM users WHERE 
+        BINARY login = :login AND BINARY password = :password");
         $stmt->execute([':login' => $login, ':password' => $password]);
         if ($stmt->rowCount() > 0) {
             return true;
@@ -27,26 +28,33 @@ class Books
     public function add($data)
     {
         $i = 0;
+        $id = [];
         $arrayTable = ['name' => 0, 'genre' => 0, 'pages' => 0];
         if ((count($data, COUNT_RECURSIVE) - count($data)) > 0) {
             foreach ($data as $key => $array1) {
                 var_dump(implode("','", array_intersect_key($array1, $arrayTable)));
-                $stmt = $this->link->prepare("INSERT INTO `book`(`name`, `genre`, `pages`) VALUES (" . "'" . implode("','", array_intersect_key($array1, $arrayTable)) . "'" . ")");
+                $stmt = $this->link->prepare("INSERT INTO `book`(`name`, `genre`, `pages`) VALUES
+                (" . "'" . implode("','", array_intersect_key($array1, $arrayTable)) . "'" . ")");
                 $stmt->execute();
                 $i = $stmt->rowCount() + $i;
+                $id[$i]['id'] = $this->link->lastInsertId();
             }
-            return $i;
+            return ['data' => $id, 'message' => $i . ' added books', 'status' => 200];
         }
-        $stmt = $this->link->prepare("INSERT INTO `book`(`name`, `genre`, `pages`) VALUES (" . "'" . implode("','", array_intersect_key($data, $arrayTable)) . "'" . ")");
+        $stmt = $this->link->prepare("INSERT INTO `book`(`name`, `genre`, `pages`) VALUES
+        (" . "'" . implode("','", array_intersect_key($data, $arrayTable)) . "'" . ")");
         $stmt->execute();
-        var_dump($stmt->rowCount());
+        $id =  $this->link->lastInsertId();
+        $i = $stmt->rowCount();
+        return ['data' => 'id=' . $id, 'message' => $i . ' added book ', 'status' => 200];
     }
-    public function update($data, $url)
+    public function update($data, $id = null)
     {
-        if (preg_match("[^/api/books(|/)$]", $url)) {
-            $arrayTable = ['id' => 0, 'name' => 0, 'genre' => 0, 'pages' => 0];
-            $i = 0;
+        if ($id == null) {
             if ((count($data, COUNT_RECURSIVE) - count($data)) > 0) {
+                $arrayTable = ['id' => 0, 'name' => 0, 'genre' => 0, 'pages' => 0];
+                $i = 0;
+                $changes = [];
                 foreach ($data as $key => $array1) {
                     $array1 = (array_intersect_key($array1, $arrayTable));
                     $id = array_shift($array1);
@@ -56,60 +64,72 @@ class Books
                     }
                     $set = substr($set, 0, -1);
                     $stmt = $this->link->prepare("UPDATE `book` SET $set WHERE id=$id");
-                    var_dump($stmt);
                     $b = $stmt->execute();
                     var_dump($b);
                     $i = $stmt->rowCount() + $i;
+                    if ($stmt->rowCount() > 0) {
+                        $changes[] = $set;
+                    }
                 }
+                return ['data' => $changes, 'message' => $i . ' changed books', 'status' => 200];
             }
-            return $i;
+            exit('accept only array of 2 or more books');
         }
-        if (preg_match("[^/api/books/([0-9]{1,})(|/)$]", $url)) { //пришел урл с id
+        if ((count($data, COUNT_RECURSIVE) - count($data)) == 0) {
             $arrayTable = ['name' => 0, 'genre' => 0, 'pages' => 0]; // задаем для сравнения массив
             $i = 0;
-            if ((count($data, COUNT_RECURSIVE) - count($data)) == 0) { // проверили что пришел одномерный массив
-                $array1 = (array_intersect_key($data, $arrayTable)); //сверили массив который пришел с нашим 'для сравнения'
-                $set = '';
-                foreach ($array1 as $key => $value) {                        //сформировали строку с массива для подстановки в sql запрос на место SET
-                    $set = $set . $key . '=' . "'" . $value . "'" . ",";
-                }
-                $set = substr($set, 0, -1); // убрали запятую в конце строки для подстановки SET в sql запрос, которая появилась после последней итерации цикла foreach
-                preg_match_all('/[0-9]/', $url, $matches); //выделили с урл номер id для sql запроса
-                $stmt = $this->link->prepare("UPDATE `book` SET $set WHERE id=" . implode($matches[0]));
-                var_dump($stmt);
-                $b = $stmt->execute();
-                var_dump($b);
-                $i = $stmt->rowCount() + $i;
+            $changes = [];
+            $array1 = (array_intersect_key($data, $arrayTable)); //сверили массив который пришел с нашим 'для сравнения'
+            $set = '';
+            foreach ($array1 as $key => $value) {                        //сформировали строку с массива для подстановки в sql запрос на место SET
+                $set = $set . $key . '=' . "'" . $value . "'" . ",";
             }
-            return $i;
+            $set = substr($set, 0, -1); // убрали запятую в конце строки для подстановки SET в sql запрос, которая появилась после последней итерации цикла foreach
+            $stmt = $this->link->prepare("UPDATE `book` SET $set 
+                WHERE id=" . $id);
+            $b = $stmt->execute();
+            var_dump($b);
+            var_dump($set);
+            $i = $stmt->rowCount() + $i;
+            if ($i > 0) {
+                $changes[] = $set;
+            }
+            return ['data' => $changes, 'message' => $i . ' changed books', 'status' => 200];
         }
     }
-    public function del($url)
+    public function del($id)
     {
-        preg_match_all('/[0-9]/', $url, $matches); //выделили с урл номер id для sql запроса
-        $stmt = $this->link->prepare("DELETE FROM `book` WHERE id=" . implode($matches[0]));
+        $stmt = $this->link->prepare("DELETE FROM `book` WHERE id=" . $id);
         $b = $stmt->execute();
         var_dump($b);
-        $i=0;
+        $i = 0;
         $i = $stmt->rowCount() + $i;
-        return $i;
+        return ['data' => 'id=' . $id, 'message' => $i . ' book is deleted', 'status' => 200];;
     }
     public function allBooks()
     {
         $query = "SELECT * FROM book";
         $resQuery = $this->link->query($query);
-        echo json_encode($resQuery->fetchAll(PDO::FETCH_ASSOC));
+        $data = $resQuery->fetchAll(PDO::FETCH_ASSOC);
+        return ['data' => $data, 'message' => 'all books', 'status' => 200];
     }
     public function booksId($id)
     {
         $query = "SELECT * FROM book WHERE id = $id";
         $resQuery = $this->link->query($query);
-        echo json_encode($resQuery->fetch(PDO::FETCH_ASSOC));
+        $data = $resQuery->fetch(PDO::FETCH_ASSOC);
+        return ['data' => $data, 'message' => 'book', 'status' => 200];
     }
     public function count()
     {
         $query = "SELECT COUNT(*) FROM book";
         $resQuery = $this->link->query($query);
-        echo $resQuery->fetchColumn();
+        return ['data' => $resQuery->fetchColumn(), 'message' => 'count of books', 'status' => 200];
+    }
+    public function response($input)
+    {
+        $resp = ['data' => $input['data'], 'message' => $input['message'], 'status' => $input['status']];
+        echo json_encode($resp);
+        exit;
     }
 }
