1f131ec051f0d2523ce6301cc1499ae09bd46682
Slushatel2019 <sluhach.nadpsu@gmail.com>
2019-10-11 19:45:21 +0300

    updated

--- Common.php
+++ Common.php
@@ -2,13 +2,6 @@
 function response($input)
 {
     #error_reporting(0);
-    if (is_array($input[0])) {
-        foreach ($input as $array) {
-            $resp = ['data' => $array['data'], 'message' => $array['message'], 'status' => $array['status']];
-            echo json_encode($resp);
-        }
-        exit;
-    }
     $resp = ['data' => $input['data'], 'message' => $input['message'], 'status' => $input['status']];
     echo json_encode($resp);
     exit;
@@ -17,3 +10,4 @@ function get($value, $default = null)
 {
     return isset($value) ? $value : $default;
 }
+
